42d206ca9dd26e1c8eb4a2e8805619079eff509b
adityaanilshukla <adityaanilsindhunath@gmail.com>
2023-08-07 23:13:32 +0800

    add navbar and footer, add all basic packages, add style, add basic page routing on index.js

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+const sqlite3 = require('sqlite3').verbose();
+
+
+//set the app to use ejs for rendering
+app.set('view engine', 'ejs');
+
+app.use(express.static("public"));
+
+app.get('/', (req, res) => {
+    res.render('home');
+})
+
+app.get('/points', (req, res) => {
+    res.render('points');
+})
+
+app.get('/planning', (req, res) => {
+    res.render('planning');
+})
+
+app.get('/expenses', (req, res) => {
+    res.render('expenses');
+})
+
+app.get('/community', (req, res) => {
+    res.render('community');
+})
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
