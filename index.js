340ca9d0f77a17c12f421643e8e86739d18c2adb
adityaanilshukla <adityaanilsindhunath@gmail.com>
2023-09-16 16:51:33 +0800

    add proper route for login and register.ejs

--- index.js
+++ index.js
@@ -64,6 +64,54 @@ app.get("/cardRec", (req, res) => {
   res.render("cardRec");
 });
 
+//post routes
+//login post route
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+  // Retrieve the hashed password from the SQLite database
+  db.get("SELECT * FROM users WHERE username = ?", [username], (err, row) => {
+    if (err || !row) {
+      // Authentication failed
+      res.send("Login failed. Please try again.");
+    } else {
+      // Compare the provided password with the stored hashed password
+      bcrypt.compare(password, row.password, (bcryptErr, result) => {
+        if (bcryptErr || !result) {
+          // Authentication failed
+          res.send("Login failed. Please try again.");
+        } else {
+          // Authentication succeeded
+          res.send("Login successful.");
+        }
+      });
+    }
+  });
+});
+
+// Register a new user
+app.post("/register", (req, res) => {
+  const { username, password } = req.body;
+  // Hash the password
+  bcrypt.hash(password, saltRounds, (hashErr, hash) => {
+    if (hashErr) {
+      res.send("Registration failed. Please try again.");
+    } else {
+      // Store the username and hashed password in the database
+      db.run(
+        "INSERT INTO users (username, password) VALUES (?, ?)",
+        [username, hash],
+        (insertErr) => {
+          if (insertErr) {
+            res.send("Registration failed. Please try again.");
+          } else {
+            res.send("Registration successful.");
+          }
+        },
+      );
+    }
+  });
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
