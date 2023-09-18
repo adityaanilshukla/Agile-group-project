4d0d6490c471f09c8bc5f518d249eacc9c4f9473
adityaanilshukla <adityaanilsindhunath@gmail.com>
2023-09-18 11:27:57 +0800

    improve the hardcoded projection.ejs page to better fit expenseByVendor table

--- public/scripts/graph.js
+++ public/scripts/graph.js
@@ -1,95 +1,3 @@
-function plotInflationGraph(userExpenses, compoundedExpenses, inflation) {
-  const currentYear = new Date().getFullYear();
-  const years = [];
-  for (let i = 0; i < 5; i++) {
-    years.push(currentYear + i);
-  }
-
-  // Calculate compounded expenses with inflation
-  for (let i = 1; i < compoundedExpenses.length; i++) {
-    compoundedExpenses[i] = compoundedExpenses[i - 1] * (1 + inflation);
-  }
-
-  new Chart("myChart", {
-    type: "line",
-    data: {
-      labels: years,
-      datasets: [
-        {
-          data: compoundedExpenses,
-          label: "Projected Expenses (5% Inflation)",
-          borderColor: "red",
-          fill: false,
-        },
-        {
-          data: userExpenses,
-          label: "Your current expenses",
-          borderColor: "blue",
-          fill: false,
-        },
-      ],
-    },
-    options: {
-      legend: {
-        display: true,
-      },
-      scales: {
-        yAxes: [
-          {
-            ticks: {
-              beginAtZero: false,
-              callback: function(value, index, values) {
-                return "$" + value;
-              },
-            },
-          },
-        ],
-      },
-      tooltips: {
-        mode: "index",
-        intersect: false,
-        callbacks: {
-          label: function(tooltipItem, data) {
-            const datasetLabel =
-              data.datasets[tooltipItem.datasetIndex].label || "";
-            const value = "$" + tooltipItem.yLabel.toFixed(2);
-            return datasetLabel + ": " + value;
-          },
-        },
-      },
-      title: {
-        display: true,
-        text: "Expense Projection Over 5 Years",
-        fontSize: 16,
-      },
-      scales: {
-        xAxes: [
-          {
-            scaleLabel: {
-              display: true,
-              labelString: "Years",
-            },
-          },
-        ],
-        yAxes: [
-          {
-            scaleLabel: {
-              display: true,
-              labelString: "Expenses ($)",
-            },
-            ticks: {
-              beginAtZero: false,
-              callback: function(value, index, values) {
-                return "$" + value;
-              },
-            },
-          },
-        ],
-      },
-    },
-  });
-}
-
 function plotIncomeExpensesGraph(
   userExpenses,
   compoundedExpenses,
@@ -184,7 +92,7 @@ function plotIncomeExpensesGraph(
             },
             ticks: {
               beginAtZero: true, // Start y-axis at 0 for a bar graph
-              callback: function(value, index, values) {
+              callback: function (value, index, values) {
                 return "$" + value;
               },
             },
@@ -198,7 +106,7 @@ function plotIncomeExpensesGraph(
             },
             ticks: {
               beginAtZero: true,
-              callback: function(value, index, values) {
+              callback: function (value, index, values) {
                 return "$" + value;
               },
             },
@@ -215,7 +123,7 @@ function plotIncomeExpensesGraph(
         mode: "index",
         intersect: false,
         callbacks: {
-          label: function(tooltipItem, data) {
+          label: function (tooltipItem, data) {
             const datasetLabel =
               data.datasets[tooltipItem.datasetIndex].label || "";
             const value = "$" + tooltipItem.yLabel.toFixed(2);
