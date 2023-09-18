588e203b6e582e6ee653b811c3cfcbd92b76fb12
adityaanilshukla <adityaanilsindhunath@gmail.com>
2023-09-18 15:15:45 +0800

    working community threads and post

--- index.js
+++ index.js
@@ -64,8 +64,23 @@ app.get("/expenses", requireAuth, (req, res) => {
   res.render("expenses");
 });
 
-app.get("/community", requireAuth, (req, res) => {
-  res.render("community");
+// retrieve threads and posts
+app.get("/community", (req, res) => {
+  db.all("SELECT * FROM threads ORDER BY id DESC", (err, threads) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send("Error retrieving threads.");
+    } else {
+      db.all("SELECT * FROM posts", (err, posts) => {
+        if (err) {
+          console.error(err);
+          res.status(500).send("Error retrieving posts.");
+        } else {
+          res.render("community", { threads, posts });
+        }
+      });
+    }
+  });
 });
 
 app.get("/savings-plan", requireAuth, (req, res) => {
@@ -229,6 +244,36 @@ app.post("/register", (req, res) => {
   });
 });
 
+// create a new thread
+app.post("/threads", (req, res) => {
+  const { title } = req.body;
+  db.run("INSERT INTO threads (title) VALUES (?)", [title], function(err) {
+    if (err) {
+      console.error(err);
+      res.status(500).send("Error creating a new thread.");
+    } else {
+      res.redirect("community");
+    }
+  });
+});
+
+// create a new post in a thread
+app.post("/posts", (req, res) => {
+  const { thread_id, content } = req.body;
+  db.run(
+    "INSERT INTO posts (thread_id, content) VALUES (?, ?)",
+    [thread_id, content],
+    function(err) {
+      if (err) {
+        console.error(err);
+        res.status(500).send("Error creating a new post.");
+      } else {
+        res.redirect("community");
+      }
+    },
+  );
+});
+
 // Create tables if they don't exist
 db.serialize(() => {
   // Create 'users' table
@@ -283,8 +328,26 @@ db.serialize(() => {
       FOREIGN KEY (userId) REFERENCES users (id)
     )
   `);
-});
 
+  // Create 'threads' table
+  db.run(`
+    CREATE TABLE IF NOT EXISTS threads (
+      id INTEGER PRIMARY KEY,
+      title TEXT
+    )
+  `);
+
+  // Create 'posts' table
+  db.run(`
+    CREATE TABLE IF NOT EXISTS posts (
+      id INTEGER PRIMARY KEY,
+      thread_id INTEGER,
+      content TEXT,
+      Field4 INTEGER,
+      FOREIGN KEY (Field4) REFERENCES threads (id)
+    )
+  `);
+});
 createDemoUser();
 
 app.listen(port, () => {
