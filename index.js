b360d56b8d2334bc3144882804557a86ee6c7dda
adityaanilshukla <adityaanilsindhunath@gmail.com>
2023-09-18 15:27:03 +0800

    prevent user from accessing community page without being logged in

--- index.js
+++ index.js
@@ -3,42 +3,34 @@ const sqlite3 = require("sqlite3");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const bcrypt = require("bcrypt");
-const session = require("express-session"); // Added session support
-const saltRounds = 10; // The number of salt rounds to use
+const session = require("express-session");
+const saltRounds = 10;
 const app = express();
 const port = 3000;
 
-// Import the createDemoUser function
 const createDemoUser = require("./public/scripts/demoUser");
 const expenseFormatter = require("./public/scripts/expenseFormatter");
 
-// Configure SQLite database
 const db = new sqlite3.Database("database.db");
 
-// Parse incoming form data
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// Set up sessions
 app.use(
   session({
-    secret: "secret", // Change this to a secret key for session encryption
+    secret: "secret",
     resave: false,
     saveUninitialized: true,
   }),
 );
 
-//set the app to use ejs for rendering
 app.set("view engine", "ejs");
 
 app.use(express.static("public"));
 
-// Middleware for checking authentication
 function requireAuth(req, res, next) {
   if (req.session.userId) {
-    // User is authenticated, proceed to the next route handler
     next();
   } else {
-    // User is not authenticated, redirect to the login page
     res.redirect("/");
   }
 }
@@ -51,7 +43,6 @@ app.get("/register", (req, res) => {
   res.render("register");
 });
 
-// Protected routes
 app.get("/home", requireAuth, (req, res) => {
   res.render("home");
 });
@@ -64,8 +55,7 @@ app.get("/expenses", requireAuth, (req, res) => {
   res.render("expenses");
 });
 
-// retrieve threads and posts
-app.get("/community", (req, res) => {
+app.get("/community", requireAuth, (req, res) => {
   db.all("SELECT * FROM threads ORDER BY id DESC", (err, threads) => {
     if (err) {
       console.error(err);
@@ -88,8 +78,7 @@ app.get("/savings-plan", requireAuth, (req, res) => {
 });
 
 app.get("/projection", requireAuth, (req, res) => {
-  // Fetch data from the database (userData and expenseByVendor)
-  const userId = req.session.userId; // Get the user's ID from the session
+  const userId = req.session.userId;
 
   db.get(
     "SELECT * FROM userData WHERE userId = ?",
@@ -101,7 +90,6 @@ app.get("/projection", requireAuth, (req, res) => {
         return;
       }
 
-      // Fetch expenseByVendor data for the user
       db.all(
         "SELECT * FROM expenseByVendor WHERE userId = ?",
         [userId],
@@ -115,14 +103,12 @@ app.get("/projection", requireAuth, (req, res) => {
             return;
           }
 
-          // Format the expenseByVendor data using the function from the module
           const formattedExpenseByVendor =
             expenseFormatter.formatExpenseByVendorData(
               userDataRow,
               expenseByVendorData,
             );
 
-          // Render the projection.ejs template with the fetched and formatted data
           res.render("projection", {
             userData: userDataRow,
             expenseByVendor: formattedExpenseByVendor,
@@ -165,86 +151,50 @@ app.get("/enter-expenses", requireAuth, (req, res) => {
   res.render("enter-expenses");
 });
 
-//post routes
 app.post("/login", (req, res) => {
   const { email, password } = req.body;
 
-  // Retrieve the hashed password from the SQLite database
-
-  db.get(
-    "SELECT * FROM users WHERE email = ?",
-
-    [email],
-
-    (err, row) => {
-      if (err || !row) {
-        // Authentication failed
-
-        res.send("Login failed. Please try again.");
-      } else {
-        // Compare the provided password with the stored hashed password
-
-        bcrypt.compare(password, row.password, (bcryptErr, result) => {
-          if (bcryptErr || !result) {
-            // Authentication failed
-
-            res.send("Login failed. Please try again.");
-          } else {
-            // Authentication succeeded
-            // Store user information in the session
-            req.session.userId = row.id;
-
-            res.redirect("/home"); // Redirect to the home page
-          }
-        });
-      }
-    },
-  );
+  db.get("SELECT * FROM users WHERE email = ?", [email], (err, row) => {
+    if (err || !row) {
+      res.send("Login failed. Please try again.");
+    } else {
+      bcrypt.compare(password, row.password, (bcryptErr, result) => {
+        if (bcryptErr || !result) {
+          res.send("Login failed. Please try again.");
+        } else {
+          req.session.userId = row.id;
+          res.redirect("/home");
+        }
+      });
+    }
+  });
 });
-
-// Handle registration form submission
 
 app.post("/register", (req, res) => {
   const { email, password } = req.body;
 
-  // Hash the password using bcrypt
-
   bcrypt.hash(password, saltRounds, (hashErr, hashedPassword) => {
     if (hashErr) {
-      // Handle the error (e.g., log it or send an error response)
-
       console.error(hashErr);
-
       res.status(500).send("Registration failed. Please try again later.");
-
       return;
     }
 
-    // Insert the user into the database
-
     db.run(
       "INSERT INTO users (email, password) VALUES (?, ?)",
-
       [email, hashedPassword],
-
       (insertErr) => {
         if (insertErr) {
-          // Handle the error (e.g., log it or send an error response)
-
           console.error(insertErr);
-
           res.status(500).send("Registration failed. Please try again later.");
         } else {
-          // Registration successful
-
-          res.render("login"); // Redirect to the login page
+          res.render("login");
         }
       },
     );
   });
 });
 
-// create a new thread
 app.post("/threads", (req, res) => {
   const { title } = req.body;
   db.run("INSERT INTO threads (title) VALUES (?)", [title], function(err) {
@@ -257,7 +207,6 @@ app.post("/threads", (req, res) => {
   });
 });
 
-// create a new post in a thread
 app.post("/posts", (req, res) => {
   const { thread_id, content } = req.body;
   db.run(
@@ -274,7 +223,6 @@ app.post("/posts", (req, res) => {
   );
 });
 
-// Create tables if they don't exist
 db.serialize(() => {
   // Create 'users' table
   db.run(`
@@ -348,6 +296,7 @@ db.serialize(() => {
     )
   `);
 });
+
 createDemoUser();
 
 app.listen(port, () => {
